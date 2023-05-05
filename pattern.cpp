c3a3e740785e0bcf41a5960ba41dda0352f1565b
kumawatkamal2698 <kumawatkamal2698@gmail.com>
2023-05-05 21:44:10 +0530

    first commit

---
+++ pattern.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+    int i,j,k,l,n;
+    int main(){
+    cout<<"enter the rows : ";
+    cin>>n;
+    for(i=n;i>=1;i--)
+    {
+    for(j=2*i;j>=1;j--)
+    cout<<" ";
+    for(k=n;k>=i;k--)
+    cout<<k<<" ";
+    for(l=i+1;l<=n;l++)
+    cout<<l<<" ";
+    cout<<"\n"; 
+    }
+    }
